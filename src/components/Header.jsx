03dcd12bb647d8eb73093b2d393ee0b85c7f9da8
felicia <feliciawillnas@gmail.com>
2025-03-23 16:41:17 +0100

    header + hamburger menu mobile import fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,76 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import styled from "styled-components";
+
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header>
-      <nav>
+    <Main>
+      <HamburgerIcon onClick={() => setOpen(!open)} />
+      <Nav open={open}>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <a href="/" onClick={() => setOpen(false)}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="/contact" onClick={() => setOpen(false)}>
+              Contact
+            </a>
           </li>
           <li>
-            <a href="/resume">Resume</a>
+            <a href="/resume" onClick={() => setOpen(false)}>
+              Resume
+            </a>
           </li>
           <li>
-            <a href="/works">Works</a>
+            <a href="/works" onClick={() => setOpen(false)}>
+              Works
+            </a>
           </li>
         </ul>
-      </nav>
-    </header>
+      </Nav>
+    </Main>
   );
 }
+
+const Main = styled.header`
+  position: absolute;
+  top: 0;
+  padding: 3rem;
+  font-family: "Source Code Pro", monospace;
+  letter-spacing: 0.04rem;
+  font-size: 14px;
+  font-weight: 400;
+  text-transform: uppercase;
+`;
+
+const Nav = styled.nav`
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 3rem;
+
+    @media (max-width: 768px) {
+      display: ${({ open }) => (open ? "flex" : "none")};
+      flex-direction: column;
+      margin-top: 2rem;
+    }
+  }
+
+  a {
+    color: #000;
+  }
+`;
+
+const HamburgerIcon = styled(GiHamburgerMenu)`
+  display: none;
+  font-size: 1rem;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
