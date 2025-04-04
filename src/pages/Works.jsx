4b3d7294989c3f27649ece9904c65f418095189d
felicia <feliciawillnas@gmail.com>
2025-04-04 18:52:49 +0200

    added links and image hover

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiExternalLinkFill } from "react-icons/ri";
 import styled from "styled-components";
 import projectData from "../data/projectData.json";
 
@@ -10,20 +11,36 @@ export default function Works() {
           <ProjectWrapper>
             <FlexedDiv>
               <h4>{project.title}</h4>
+              <h4>&nbsp;</h4>
               <h4>Tech stack</h4>
-              <h4>Info</h4>
+              <h4>Details</h4>
             </FlexedDiv>
             <FlexedDiv>
-              <a href="/">
-                <p>Visit site</p>
-              </a>
+              <li>
+                <a href={project.website_link}>
+                  View site
+                  <RiExternalLinkFill />
+                </a>
+              </li>
+              <li>
+                <a href={project.github_link}>
+                  View on github
+                  <RiExternalLinkFill />
+                </a>
+              </li>
               <p>{project.tech}</p>
               <p>{project.text}</p>
             </FlexedDiv>
           </ProjectWrapper>
-          <ProjectImage>
+          <ProjectImageWrapper>
             <img src={project.image} alt="image not found" />
-          </ProjectImage>
+            <ProjectImageText>
+              <a href="/">
+                Visit page
+                <RiExternalLinkFill />
+              </a>
+            </ProjectImageText>
+          </ProjectImageWrapper>
         </div>
       ))}
     </Main>
@@ -37,26 +54,66 @@ const Main = styled.main`
   gap: 5rem;
   padding: 1rem;
   margin-top: 5rem;
+
+  @media (max-width: 768px) {
+    gap: 3rem;
+    padding: 0.7rem;
+  }
 `;
 
-const ProjectImage = styled.div`
+const ProjectImageText = styled.div`
+  visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 2px;
+
+  a {
+    display: flex;
+    color: #f0f0f0;
+    gap: 0.2rem;
+    align-items: center;
+  }
+`;
+
+const ProjectImageWrapper = styled.div`
+  position: relative;
+  margin-top: 0.5rem;
+
   img {
-    width: 10vw;
+    width: 100%;
     height: auto;
     object-fit: cover;
     border-radius: 2px;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+
+  &:hover ${ProjectImageText} {
+    visibility: visible;
   }
 `;
 
 const ProjectWrapper = styled.div`
   display: flex;
-  /* justify-content: space-between; */
   gap: 8rem;
+
+  @media (max-width: 768px) {
+    gap: 3rem;
+  }
 `;
 
 const FlexedDiv = styled.div`
   display: flex;
   flex-direction: column;
+
+  li {
+    :hover {
+      background-color: ${({ theme }) => theme.secondary};
+      color: ${({ theme }) => theme.primary};
+    }
+  }
 
   h4 {
     text-transform: uppercase;
