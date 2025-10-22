caff6be2f8940dffa8c760cd15220f9fa639308f
felicia <feliciawillnas@gmail.com>
2025-10-22 23:59:42 +0200

    clean up home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,76 +4,30 @@ export default function Home() {
   return (
     <>
       <Main>
-        <InnerBox>
-          <Title>Portfolio</Title>
-          <Titles>{/* <h4>Something</h4> */}</Titles>
-        </InnerBox>
+        <Title>Portfolio</Title>
       </Main>
     </>
   );
 }
-
-const Title = styled.h1`
-  /* width: 100%; */
-  width: 100vw;
-  /* font-size: 100vw; */
-  font-size: 17.4vmax;
-  color: ${({ theme }) => theme.text};
-  letter-spacing: -0.2rem;
-  text-transform: uppercase;
-  /* font-family: Arial, Helvetica, sans-serif; */
-  font-family: inter, sans-serif;
-
-  line-height: 0.9;
-
-  @media (max-width: 768px) {
-    font-size: 20px;
-  }
-`;
 
 const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  /* height: 100vh; */
-  padding: 5rem 1rem 0 1rem;
-  background-color: ${({ theme }) => theme.primary};
-
-  /* h1 {
-    font-size: 10vmax;
-    font-family: Arial, Helvetica, sans-serif;
-  } */
+  padding: 6rem 1rem 0 1rem;
 `;
 
-const InnerBox = styled.div`
+const Title = styled.h1`
   width: 100vw;
-  height: 80vh;
-  border-radius: 1px;
-  background-color: ${({ theme }) => theme.primary};
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-auto-columns: 1fr;
-  color: white;
-`;
-
-const Titles = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* gap: 0.5rem; */
+  font-size: 17.4vmax;
+  color: ${({ theme }) => theme.text};
+  letter-spacing: -0.2rem;
   text-transform: uppercase;
+  font-family: inter, sans-serif;
+  line-height: 0.9;
 
-  h2 {
-    font-size: 5rem;
+  @media (max-width: 768px) {
+    font-size: 20px;
   }
-`;
-
-const Information = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* gap: 0.5rem; */
-  text-transform: uppercase;
 `;
